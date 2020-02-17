2d3821963d20ca8dceb4bddd13b31ba56c2e60b6
aleitner <leitnersalex@gmail.com>
2020-02-17 05:44:44 -0500

    Initial streaming example

---
+++ go.mod
@@ -0,0 +1,9 @@
+module grpc-bidirectional-stream
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.3
+	github.com/urfave/cli v1.22.2
+	google.golang.org/grpc v1.27.1
+)
---
+++ server/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pb "grpc-bidirectional-stream/proto"
+)
+
+var (
+	port int
+)
+
+type sampleServer struct {
+}
+
+func initializeFlags() {
+	flag.IntVar(&port, "port", 8080, "port")
+	flag.Parse()
+}
+
+func (s *sampleServer) SampleBidirectional(stream pb.Sample_SampleBidirectionalServer) error {
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		fmt.Println("received", res.GetData())
+		err = stream.Send(&pb.SampleBidirectionalResponse{
+			Data: res.GetData(),
+		})
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	initializeFlags()
+
+	// start grpc server
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Println("failed")
+		os.Exit(1)
+	}
+	defer lis.Close()
+
+	grpcServer := grpc.NewServer()
+
+	var s sampleServer
+	pb.RegisterSampleServer(grpcServer, &s)
+
+	defer grpcServer.GracefulStop()
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		fmt.Println("failed")
+		os.Exit(1)
+	}
+}
